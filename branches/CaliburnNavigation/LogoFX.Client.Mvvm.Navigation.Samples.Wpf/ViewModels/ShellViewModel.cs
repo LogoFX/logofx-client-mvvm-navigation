d7dc1e41eef243c5442130e232e6a4d9d9b9545f
dmitrybublik <dmitry.buba@gmail.com>
2016-06-14 14:44:37 +0300

    Fixed CM version.

--- branches/CaliburnNavigation/LogoFX.Client.Mvvm.Navigation.Samples.Wpf/ViewModels/ShellViewModel.cs
+++ branches/CaliburnNavigation/LogoFX.Client.Mvvm.Navigation.Samples.Wpf/ViewModels/ShellViewModel.cs
@@ -1,9 +1,15 @@
 ﻿using Caliburn.Micro;
+using JetBrains.Annotations;
 
 namespace LogoFX.Client.Mvvm.Navigation.Samples.Wpf.ViewModels
 {
+    [UsedImplicitly]
     public class ShellViewModel : Screen
     {
-        
+        public override string DisplayName
+        {
+            get { return "Navigation Sample - WPF Application"; }
+            set { }
+        }
     }
 }
